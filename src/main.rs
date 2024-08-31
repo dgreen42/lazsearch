3c594a92ec51651f817853e6f0c3104f0254eb20
laz <lazgreen32@gmail.com>
2024-08-31 12:49:36 -0400

    completed recursive search algo

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,43 @@
-use std::{
-    env,
-    error::Error,
-    ffi::OsStr,
-    fs::{read_dir, DirEntry, ReadDir},
-    path::PathBuf,
-};
+use std::{env, path::PathBuf};
 
 fn main() {
     let search = env::args().nth(1).expect("Enter search");
-    let result = String::new();
     let top_dir = env::current_dir().expect("Directory not found");
 
-    for one_down in top_dir.read_dir().unwrap() {
-        search_dir(one_down.unwrap().path().to_str().unwrap(), &search);
-    }
+    quick_search(&top_dir, &search);
 }
 
 fn search_dir(dir: &str, search: &str) -> Option<String> {
-    println!("{:?}", dir);
     if dir.contains(search) {
         Some(dir.to_string())
     } else {
-        let path = PathBuf::new();
+        None
+    }
+}
+
+fn nothing() {}
+
+fn quick_search(top_dir: &PathBuf, search: &str) {
+    //println!("{:?}", top_dir);
+    if !top_dir.is_dir() {
+        let result = search_dir(top_dir.as_path().to_str().unwrap(), &search);
+        match result {
+            Some(..) => println!("{:?}", result.unwrap()),
+            None => nothing(),
+        }
+    } else {
+        for one_down in top_dir.read_dir().unwrap() {
+            let temp_dir = one_down.unwrap();
+            let temp_dir2 = temp_dir.path();
+            let cur_dir = temp_dir2.to_str().unwrap();
+            let result = search_dir(cur_dir, &search);
+            match result {
+                Some(..) => println!("{:?}", result.unwrap()),
+                None => {
+                    let new_top = PathBuf::from(cur_dir);
+                    quick_search(&new_top, search);
+                }
+            }
+        }
     }
 }
