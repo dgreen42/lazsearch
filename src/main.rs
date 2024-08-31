d2e10da2f36035b00cc92d543049ea5b9b082adf
laz <lazgreen32@gmail.com>
2024-08-31 14:44:48 -0400

    fixed bug searching in /

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::{env, path::PathBuf};
+use colored::Colorize;
+use std::{env, error::Error, fs::File, io::BufWriter, io::Write, path::PathBuf};
 
 fn main() {
     let search = env::args().nth(1).expect("Enter search");
     let top_dir = env::current_dir().expect("Directory not found");
 
     quick_search(&top_dir, &search);
+    println!("{}", "Done!".green());
 }
 
 fn search_dir(dir: &str, search: &str) -> Option<String> {
@@ -18,21 +20,25 @@ fn search_dir(dir: &str, search: &str) -> Option<String> {
 fn do_nothing() {}
 
 fn quick_search(top_dir: &PathBuf, search: &str) {
-    //println!("{:?}", top_dir);
     if !top_dir.is_dir() {
         let result = search_dir(top_dir.as_path().to_str().unwrap(), &search);
         match result {
-            Some(..) => println!("{:?}", result.unwrap()),
+            Some(..) => println!("{} {:?}", "Match at: ".cyan(), result.unwrap()),
             None => do_nothing(),
         }
     } else {
         for one_down in top_dir.read_dir().unwrap() {
-            let temp_dir = one_down.unwrap();
+            let temp_dir = match one_down {
+                Ok(dir) => dir,
+                Err(err) => panic!("fuck: {:?}", err),
+            };
             let temp_dir2 = temp_dir.path();
             let cur_dir = temp_dir2.to_str().unwrap();
             let result = search_dir(cur_dir, &search);
             match result {
-                Some(..) => println!("{:?}", result.unwrap()),
+                Some(..) => {
+                    println!("{} {:?}", "Match at: ".cyan(), result.unwrap());
+                }
                 None => {
                     let new_top = PathBuf::from(cur_dir);
                     quick_search(&new_top, search);
