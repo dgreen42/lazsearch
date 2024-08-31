add4779c01f8962dc2cc2b2427eee7c84f324aba
laz <lazgreen32@gmail.com>
2024-08-31 17:30:35 -0400

    go past matched node

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,27 @@ use colored::Colorize;
 use std::{env, path::PathBuf};
 
 fn main() {
-    let search = env::args().nth(1).expect("Enter search");
+    let search_pat = env::args().nth(1).expect("Enter search");
     let top_dir = env::current_dir().expect("Directory not found");
 
-    quick_search(&top_dir, &search);
+    quick_search(&top_dir, &search_pat);
     println!("{}", "Done!".green());
 }
 
-fn search_dir(dir: &str, search: &str) -> Option<String> {
-    if dir.contains(search) {
-        Some(dir.to_string())
-    } else {
-        None
+mod search {
+    pub fn search_dir(dir: &str, search: &str) -> Option<String> {
+        if dir.contains(search) {
+            Some(dir.to_string())
+        } else {
+            None
+        }
     }
+    // do absolutely nothing cause why would you want to print something if there is no match?
+    pub fn do_nothing() {}
 }
 
-// do absolutely nothing cause why would you want to print something if there is no match?
-fn do_nothing() {}
-
 fn quick_search(top_dir: &PathBuf, search: &str) {
+    use search::{do_nothing, search_dir};
     if !top_dir.is_dir() {
         let result = search_dir(top_dir.as_path().to_str().unwrap(), &search);
         match result {
@@ -39,6 +41,8 @@ fn quick_search(top_dir: &PathBuf, search: &str) {
             match result {
                 Some(..) => {
                     println!("{} {:?}", "Match at: ".cyan(), result.unwrap());
+                    let new_top = PathBuf::from(cur_dir);
+                    quick_search(&new_top, search);
                 }
                 None => {
                     let new_top = PathBuf::from(cur_dir);
