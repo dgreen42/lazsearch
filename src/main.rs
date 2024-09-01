34fba0fe25fa2ed396635508b0266e5c5b80dc0d
laz <lazgreen32@gmail.com>
2024-08-31 20:37:36 -0400

    used some structs and methods

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
 use colored::Colorize;
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, thread};
 
 fn main() {
     let search_pat = env::args().nth(1).expect("Enter search");
     let top_dir = env::current_dir().expect("Directory not found");
-
     quick_search(&top_dir, &search_pat);
     println!("{}", "Done!".green());
 }
-
 mod search {
-    pub fn search_dir(dir: &str, search: &str) -> Option<String> {
-        if dir.contains(search) {
-            Some(dir.to_string())
-        } else {
-            None
+    use std::path::PathBuf;
+    pub struct Searcher {
+        pub directory: String,
+        pub search: String,
+    }
+    pub enum Recurse {
+        Base,
+        Continue(Box<Searcher>),
+    }
+    impl Searcher {
+        pub fn search(&self) -> Option<String> {
+            if self.directory.contains(&self.search) {
+                Some(self.directory.to_string())
+            } else {
+                None
+            }
         }
     }
-    // do absolutely nothing cause why would you want to print something if there is no match?
     pub fn do_nothing() {}
 }
-
 fn quick_search(top_dir: &PathBuf, search: &str) {
-    use search::{do_nothing, search_dir};
+    use search::*;
+    let top_dir_s = top_dir.as_path().to_str().unwrap().to_string();
     if !top_dir.is_dir() {
-        let result = search_dir(top_dir.as_path().to_str().unwrap(), &search);
+        let searcher = Searcher {
+            directory: top_dir_s,
+            search: search.to_string(),
+        };
+        let result = searcher.search();
         match result {
             Some(..) => println!("{} {:?}", "Match at: ".cyan(), result.unwrap()),
             None => do_nothing(),
@@ -36,13 +48,15 @@ fn quick_search(top_dir: &PathBuf, search: &str) {
                 Err(err) => panic!("fuck: {:?}", err),
             };
             let temp_dir2 = temp_dir.path();
-            let cur_dir = temp_dir2.to_str().unwrap();
-            let result = search_dir(cur_dir, &search);
+            let cur_dir = temp_dir2.to_str().unwrap().to_string();
+            let searcher = Searcher {
+                directory: cur_dir.clone(),
+                search: search.to_string(),
+            };
+            let result = searcher.search();
             match result {
                 Some(..) => {
                     println!("{} {:?}", "Match at: ".cyan(), result.unwrap());
-                    let new_top = PathBuf::from(cur_dir);
-                    quick_search(&new_top, search);
                 }
                 None => {
                     let new_top = PathBuf::from(cur_dir);
